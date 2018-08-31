d2aba2a9d7a8291ddb73db08455c13e5fe5f6d03
jiangjianshi <jiangjianshi001@163.com>
2018-08-31 23:55:47 +0800

    jj

--- clzmall-common/src/main/java/com/clzmall/common/util/HttpUtil.java
+++ clzmall-common/src/main/java/com/clzmall/common/util/HttpUtil.java
@@ -1,6 +1,5 @@
 package com.clzmall.common.util;
 
-import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
@@ -30,290 +29,292 @@ import java.util.Map.Entry;
  */
 public class HttpUtil {
 
-	private static final Logger logger = LoggerFactory.getLogger(HttpUtil.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpUtil.class);
 
-	public static String get(String url) throws Exception {
-		return get(url, null, null);
-	}
+    public static String get(String url) throws Exception {
+        return get(url, null, null);
+    }
 
-	public static String get(String url, Map<String, String> params) throws Exception {
-		return get(url, params, null);
-	}
-	
-	public static String get(String url, Map<String, String> params, String charset) throws Exception {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
+    public static String get(String url, Map<String, String> params) throws Exception {
+        return get(url, params, null);
+    }
 
-		try {
-			if (MapUtils.isNotEmpty(params)) {
-				if (!url.contains("?")) {
-					url += "?";
-				}
+    public static String get(String url, Map<String, String> params, String charset) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
 
-				if (!url.endsWith("&") && !url.endsWith("?")) {
-					url += "&";
-				}
-				url = String.format("%s%s", url, toQueryString(params));
-			}
+        try {
+            if (params != null && !params.isEmpty()) {
+                if (!url.contains("?")) {
+                    url += "?";
+                }
 
-			HttpGet httpGet = new HttpGet(url);
-			
-			//避免出现超时，如果不配置，httpClient4.3以后默认>24小时
-			httpGet.setConfig(getReqeustConfig());
-			
-			// logger.info("[GET]request sent, url=" + url);
+                if (!url.endsWith("&") && !url.endsWith("?")) {
+                    url += "&";
+                }
+                url = String.format("%s%s", url, toQueryString(params));
+            }
 
-			long start = System.currentTimeMillis();
+            HttpGet httpGet = new HttpGet(url);
 
-			response = httpClient.execute(httpGet);
-			HttpEntity responseEntity = response.getEntity();
+            //避免出现超时，如果不配置，httpClient4.3以后默认>24小时
+            httpGet.setConfig(getReqeustConfig());
 
-			long cost = System.currentTimeMillis() - start;
-			String result = null;
-			if (StringUtils.isEmpty(charset)) {
-				result = EntityUtils.toString(responseEntity);
-			} else {
-				result = EntityUtils.toString(responseEntity, charset);
-			}
-			
-			// logger.info("[GET]response received, url=" + url + "cost=" + cost
-			// + ", result=" + result);
+            // logger.info("[GET]request sent, url=" + url);
 
-			return result;
-		} finally {
-			try {
-				httpClient.close();
+            long start = System.currentTimeMillis();
 
-				if (null != response) {
-					response.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
+            response = httpClient.execute(httpGet);
+            HttpEntity responseEntity = response.getEntity();
 
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @return
-	 * @throws Exception
-	 */
-	public static String post(String url, Map<String, String> params) throws Exception {
-		return post(url, params, null);
-	}
+            long cost = System.currentTimeMillis() - start;
+            String result = null;
+            if (StringUtils.isEmpty(charset)) {
+                result = EntityUtils.toString(responseEntity);
+            } else {
+                result = EntityUtils.toString(responseEntity, charset);
+            }
 
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @return
-	 * @throws Exception
-	 */
-	public static String postFile(String url, Map<String, Object> params) throws Exception {
-		return postFile(url, params, null);
-	}
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @param headers
-	 * @return
-	 * @throws Exception
-	 */
-	public static String post(String url, Map<String, String> params, Map<String, String> headers) throws Exception {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
+            // logger.info("[GET]response received, url=" + url + "cost=" + cost
+            // + ", result=" + result);
 
-		try {
-			HttpPost httpPost = new HttpPost(url);
-			if (MapUtils.isNotEmpty(headers)) {
-				for (Entry<String, String> entry : headers.entrySet()) {
-					httpPost.setHeader(entry.getKey(), entry.getValue());
-				}
-			}
+            return result;
+        } finally {
+            try {
+                httpClient.close();
 
-			List<NameValuePair> httpParams = new ArrayList<>();
-			for (Entry<String, String> entry : params.entrySet()) {
-				httpParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-			}
-			httpPost.setEntity(new UrlEncodedFormEntity(httpParams, "UTF-8"));
-			
-			//避免出现超时，如果不配置，httpClient4.3以后默认>24小时
-            httpPost.setConfig(getReqeustConfig()); 
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
 
-			logger.info("[POST]url=" + url + ", request param=" + params);
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public static String post(String url, Map<String, String> params) throws Exception {
+        return post(url, params, null);
+    }
 
-			//httpPost.setHeader("x-access-host", "m.huizhaofang.com");
-			 
-			response = httpClient.execute(httpPost);
-			HttpEntity responseEntity = response.getEntity();
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public static String postFile(String url, Map<String, Object> params) throws Exception {
+        return postFile(url, params, null);
+    }
 
-			String result = EntityUtils.toString(responseEntity);
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @param params
+     * @param headers
+     * @return
+     * @throws Exception
+     */
+    public static String post(String url, Map<String, String> params, Map<String, String> headers) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
 
-			logger.info("[POST]url=" + url + ", response=" + result);
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            if (headers != null && !headers.isEmpty()) {
+                for (Entry<String, String> entry : headers.entrySet()) {
+                    httpPost.setHeader(entry.getKey(), entry.getValue());
+                }
+            }
 
-			return result;
-		} finally {
-			try {
-				httpClient.close();
+            List<NameValuePair> httpParams = new ArrayList<>();
+            for (Entry<String, String> entry : params.entrySet()) {
+                httpParams.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            httpPost.setEntity(new UrlEncodedFormEntity(httpParams, "UTF-8"));
 
-				if (null != response) {
-					response.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
-
-	
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @param headers
-	 * @return
-	 * @throws Exception
-	 */
-	public static String postFile(String url, Map<String, Object> params, Map<String, String> headers) throws Exception {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
-
-		try {
-			HttpPost httpPost = new HttpPost(url);
-			if (MapUtils.isNotEmpty(headers)) {
-				for (Entry<String, String> entry : headers.entrySet()) {
-					httpPost.setHeader(entry.getKey(), entry.getValue());
-				}
-			}
-
-			List<NameValuePair> httpParams = new ArrayList<>();
-			for (Entry<String, Object> entry : params.entrySet()) {
-				httpParams.add(new BasicNameValuePair(entry.getKey(), (String) entry.getValue()));
-			}
-			httpPost.setEntity(new UrlEncodedFormEntity(httpParams, "UTF-8"));
-			
-			//避免出现超时，如果不配置，httpClient4.3以后默认>24小时
+            //避免出现超时，如果不配置，httpClient4.3以后默认>24小时
             httpPost.setConfig(getReqeustConfig());
-			
-			logger.info("[POST]url=" + url + ", request param=" + params);
 
-			response = httpClient.execute(httpPost);
-			HttpEntity responseEntity = response.getEntity();
+            logger.info("[POST]url=" + url + ", request param=" + params);
 
-			String result = EntityUtils.toString(responseEntity);
+            //httpPost.setHeader("x-access-host", "m.huizhaofang.com");
 
-			logger.info("[POST]url=" + url + ", response=" + result);
+            response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
 
-			return result;
-		} finally {
-			try {
-				httpClient.close();
+            String result = EntityUtils.toString(responseEntity);
 
-				if (null != response) {
-					response.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @return
-	 * @throws Exception
-	 */
-	public static String post(String url, String entity) throws Exception {
-		return post(url, entity, null);
-	}
+            logger.info("[POST]url=" + url + ", response=" + result);
 
-	/**
-	 * 发起post请求
-	 *
-	 * @param url
-	 * @param params
-	 * @param headers
-	 * @return
-	 * @throws Exception
-	 */
-	public static String post(String url, String entity, Map<String, String> headers) throws Exception {
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse response = null;
+            return result;
+        } finally {
+            try {
+                httpClient.close();
 
-		try {
-			HttpPost httpPost = new HttpPost(url);
-			if (MapUtils.isNotEmpty(headers)) {
-				for (Entry<String, String> entry : headers.entrySet()) {
-					httpPost.setHeader(entry.getKey(), entry.getValue());
-				}
-			}
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
 
-			StringEntity stringEntity = new StringEntity(entity, "UTF-8");
 
-			httpPost.setEntity(stringEntity);
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @param params
+     * @param headers
+     * @return
+     * @throws Exception
+     */
+    public static String postFile(String url, Map<String, Object> params, Map<String, String> headers) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
 
-			//避免出现超时，如果不配置，httpClient4.3以后默认>24小时
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            if (headers != null && !headers.isEmpty()) {
+                for (Entry<String, String> entry : headers.entrySet()) {
+                    httpPost.setHeader(entry.getKey(), entry.getValue());
+                }
+            }
+
+            List<NameValuePair> httpParams = new ArrayList<>();
+            for (Entry<String, Object> entry : params.entrySet()) {
+                httpParams.add(new BasicNameValuePair(entry.getKey(), (String) entry.getValue()));
+            }
+            httpPost.setEntity(new UrlEncodedFormEntity(httpParams, "UTF-8"));
+
+            //避免出现超时，如果不配置，httpClient4.3以后默认>24小时
             httpPost.setConfig(getReqeustConfig());
-			
-			logger.info("[POST]url=" + url + ", request stringEntity=" + stringEntity);
 
-			response = httpClient.execute(httpPost);
-			HttpEntity responseEntity = response.getEntity();
+            logger.info("[POST]url=" + url + ", request param=" + params);
 
-			String result = EntityUtils.toString(responseEntity);
+            response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
 
-			logger.info("[POST]url=" + url + ", response=" + result);
+            String result = EntityUtils.toString(responseEntity);
 
-			return result;
-		} finally {
-			try {
-				httpClient.close();
+            logger.info("[POST]url=" + url + ", response=" + result);
 
-				if (null != response) {
-					response.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
-	
-	/**
-	 * 获取要查询的字符串
-	 * @param data
-	 * @return
-	 * @throws UnsupportedEncodingException
-	 */
-	public static String toQueryString(Map<?, ?> data)
-             throws UnsupportedEncodingException {
-		StringBuilder queryString = new StringBuilder();
-		for (Entry<?, ?> pair : data.entrySet()) {
-             queryString.append(pair.getKey() + "=");
-             queryString.append(URLEncoder.encode((String) pair.getValue(),
-                             "UTF-8") + "&");
-		}
-		if (queryString.length() > 0) {
-             queryString.deleteCharAt(queryString.length() - 1);
-		}
-		return queryString.toString();
-	}
+            return result;
+        } finally {
+            try {
+                httpClient.close();
+
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public static String post(String url, String entity) throws Exception {
+        return post(url, entity, null);
+    }
+
+    /**
+     * 发起post请求
+     *
+     * @param url
+     * @param headers
+     * @return
+     * @throws Exception
+     */
+    public static String post(String url, String entity, Map<String, String> headers) throws Exception {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+
+        try {
+            HttpPost httpPost = new HttpPost(url);
+            if (headers != null && !headers.isEmpty()) {
+                for (Entry<String, String> entry : headers.entrySet()) {
+                    httpPost.setHeader(entry.getKey(), entry.getValue());
+                }
+            }
+
+            StringEntity stringEntity = new StringEntity(entity, "UTF-8");
+
+            httpPost.setEntity(stringEntity);
+
+            //避免出现超时，如果不配置，httpClient4.3以后默认>24小时
+            httpPost.setConfig(getReqeustConfig());
+
+            logger.info("[POST]url=" + url + ", request stringEntity=" + stringEntity);
+
+            response = httpClient.execute(httpPost);
+            HttpEntity responseEntity = response.getEntity();
+
+            String result = EntityUtils.toString(responseEntity);
+
+            logger.info("[POST]url=" + url + ", response=" + result);
+
+            return result;
+        } finally {
+            try {
+                httpClient.close();
+
+                if (null != response) {
+                    response.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    /**
+     * 获取要查询的字符串
+     *
+     * @param data
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static String toQueryString(Map<?, ?> data)
+            throws UnsupportedEncodingException {
+        StringBuilder queryString = new StringBuilder();
+        for (Entry<?, ?> pair : data.entrySet()) {
+            queryString.append(pair.getKey() + "=");
+            queryString.append(URLEncoder.encode((String) pair.getValue(),
+                    "UTF-8") + "&");
+        }
+        if (queryString.length() > 0) {
+            queryString.deleteCharAt(queryString.length() - 1);
+        }
+        return queryString.toString();
+    }
 
     /**
      * 获取HTTP请求的配置
+     *
      * @return
      */
-	public static RequestConfig getReqeustConfig() {
-		Integer timeout = 60000;
-		//logger.info("Timeout time is:" + timeout);
-		//1.ConnectionRequestTimeout: 经历多久之后，这次请求被视为过期 
-		//2.ConnectTimeout: 等待与服务器建立连接的时间 
-		//3.SocketTimeout: 与服务器的连接建立了，等待服务器发送数据的时间
-		RequestConfig requstConfigure = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
-		return requstConfigure;
-	}
+    public static RequestConfig getReqeustConfig() {
+        Integer timeout = 60000;
+        //logger.info("Timeout time is:" + timeout);
+        //1.ConnectionRequestTimeout: 经历多久之后，这次请求被视为过期
+        //2.ConnectTimeout: 等待与服务器建立连接的时间
+        //3.SocketTimeout: 与服务器的连接建立了，等待服务器发送数据的时间
+        RequestConfig requstConfigure = RequestConfig.custom().setSocketTimeout(timeout).setConnectTimeout(timeout).build();
+        return requstConfigure;
+    }
 }
